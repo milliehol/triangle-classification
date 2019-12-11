031a70f8330202ba70adfce227f5ae34b2c49162
Carla Holcomb <carla.programmer@gmail.com>
2019-12-11 04:36:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :equilateral, :isosceles, :scalene
+ 
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+ 
+  def kind(type)
+    self.kind = type
+    if triangle.class != triangle.valid
+      raise TriangleError
+    else
+      triangle.kind = self
+    end
+  end
+ 
+  class TriangleError < StandardError
+  end
+
 end
